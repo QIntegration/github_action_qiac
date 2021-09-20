5d10f40be4cdaab93b75d2c598cee11b25d1b463
Amol Phatangade <aphatangade@qualys.com>
2021-09-20 18:51:23 +0530

    Adding GitHub Action Code

--- README.md
+++
@@ -1,2 +0,0 @@
-# github_action_qiac
-Github Action for IaC Security
---
+++ entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/sh -l
+
+SCANFOLDER=$1
+
+echo "Action triggered by $GITHUB_EVENT_NAME event"
+
+if [ $GITHUB_EVENT_NAME = "push" ] || [ $GITHUB_EVENT_NAME = "pull_request" ]
+then
+    if [ $(git diff --name-only --diff-filter=ACMRT HEAD^ HEAD | wc -l) -eq "0" ]; then 
+        echo "There are no files/folders to scan."
+        exit 0
+    else
+        echo "From the below files, Only the files with extensions supported by IaC module are included in the scan."
+        git diff --name-only --diff-filter=ACMRT HEAD^ HEAD
+        foldername="qiacscanfolder_$(date +%Y%m%d%H%M%S)"
+        mkdir $foldername
+        cp --parents $(git diff --name-only --diff-filter=ACMRT HEAD^ HEAD) $foldername
+        cd $foldername
+        SCANFOLDER="."
+    fi
+else
+    if [ "$SCANFOLDER" = "." ]
+    then 
+        echo "Scanning entire repository."
+    else
+        echo "Scan Directory Path is - $SCANFOLDER"
+    fi
+fi
+
+ #Calling Iac CLI
+ echo "Scanning Started at - $(date +"%Y-%m-%d %H:%M:%S")"
+ qiac scan -a $URL -u $UNAME -p $PASS -d $SCANFOLDER -m json -n GitHubActionScan --tag [{\"BRANCH_NAME\":\"$GITHUB_REF\"},{\"REPOSITORY_NAME\":\"$GITHUB_REPOSITORY\"}] > /result.json
+ echo "Scanning Completed at - $(date +"%Y-%m-%d %H:%M:%S")"
+ #process result for annotation
+ echo " "
+ echo "SCAN RESULT"
+ cd /
+ cat result.json
+ python resultParser.py result.json
+
+
+
+
+
+
+
+
+
+
+
