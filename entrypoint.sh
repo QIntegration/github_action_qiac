ff84a0d99a8a82feb931c26e363f5745db6ddffe
aphatangade <58165224+aphatangade@users.noreply.github.com>
2022-02-07 20:47:40 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ if [ $GITHUB_EVENT_NAME = "push" ] || [ $GITHUB_EVENT_NAME = "pull_request" ]
 then
     if [ $(git diff --name-only --diff-filter=ACMRT HEAD^ HEAD | wc -l) -eq "0" ]; then 
         echo "There are no files/folders to scan."
-        echo "{\"version\": \"2.1.0\",\"runs\": [{\"tool\": {\"driver\": {\"name\": \"QualysIaCSecurity\",\"organization\": \"Qualys\"}},\"results\": []}]}" > ../response.sarif
+        echo "{\"version\": \"2.1.0\",\"runs\": [{\"tool\": {\"driver\": {\"name\": \"QualysIaCSecurity\",\"organization\": \"Qualys\"}},\"results\": []}]}" > response.sarif
         exit 0
     else
         echo "From the below files, Only the files with extensions supported by IaC module are included in the scan."
