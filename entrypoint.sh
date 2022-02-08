29e00a5bacf1b5b79217eabc2ac047b8550ae041
aphatangade <58165224+aphatangade@users.noreply.github.com>
2022-02-08 20:54:01 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,6 +45,7 @@ fi
     echo "Scan ID:" $SCAN_ID
     qiac getresult -a $URL -u $UNAME -p $PASS -i $SCAN_ID -m SARIF -s > /raw_result.sarif
  fi
+ ls -la ../
  if [ -f scan_response_*.sarif ]; then
      mv scan_response_*.sarif ../response.sarif
      chmod 755 ../response.sarif
