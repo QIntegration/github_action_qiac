3eccab421a0d3eb651b626860233a057405c0459
nikita-bedmutha <41138628+nikita-bedmutha@users.noreply.github.com>
2022-06-30 10:43:06 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo "Action triggered by $GITHUB_EVENT_NAME event"
 
 if [ $GITHUB_EVENT_NAME = "push" ] || [ $GITHUB_EVENT_NAME = "pull_request" ]
 then
-    if [ $(git diff --name-only --diff-filter=ACMRT HEAD~1 HEAD | wc -l) -eq "0" ]; then 
+    if [ $(git diff --name-only --diff-filter=ACMRT HEAD^ HEAD | wc -l) -eq "0" ]; then 
         echo "There are no files/folders to scan."
         echo "{\"version\": \"2.1.0\",\"runs\": [{\"tool\": {\"driver\": {\"name\": \"QualysIaCSecurity\",\"organization\": \"Qualys\"}},\"results\": []}]}" > response.sarif
         exit 0
